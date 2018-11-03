317875d02544374ac62e5d004d8dc493e29af99a
johnsquispe <johns.quispe@gmail.com>
2018-11-03 12:03:05 -0300

    first commit

---
+++ src/main/java/br/com/johnsquispe/obstaclesrest/ObstaclesRestApplication.java
@@ -0,0 +1,12 @@
+package br.com.johnsquispe.obstaclesrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObstaclesRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObstaclesRestApplication.class, args);
+	}
+}
